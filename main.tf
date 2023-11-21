be02d1f310efb54381deaba05594120468d77e41
Ashish <84917926+ag-cloudskills@users.noreply.github.com>
2023-11-22 10:23:43 +1100

    #15terraform provider conf s3 bucket creation (#16)

--- main.tf
+++ main.tf
@@ -4,7 +4,15 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
-  }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.26.0"
+   }
+ }
+}
+
+provider "aws" {
+  # Configuration options
 }
 
 provider "random" {
@@ -12,8 +20,20 @@ provider "random" {
 }
 
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
+  lower            = true
+  upper            = false
   special          = false
+}
+
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.id
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
 
 
